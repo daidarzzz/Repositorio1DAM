63e9649a659141ff745e30e1a80381b3dd02713f
daidarzzz <davidandreica127@gmail.com>
2025-10-01 14:33:19 +0200

    Añadi calculador de peso y altura y minimo y maximo

--- ejercicios_DAM/src/main/java/org/example/practicas/practica3.java
+++ ejercicios_DAM/src/main/java/org/example/practicas/practica3.java
@@ -5,12 +5,29 @@ import java.util.Scanner;
 public class practica3 {
     static void main() {
         Scanner read = new Scanner(System.in);
+        final int PESO_MAXIMO = 120;
+
         System.out.println("***¡BIENVENIDO AL RATÓN JUGUETÓN!***");
 
         System.out.println("Introduce tu altura (cm): ");
         int altura = read.nextInt();
         if (altura < 140) {
-            System.out.println();
+            int cm_restantes = 140 - altura;
+            System.out.println("LO SIENTO, NO PUEDES MONTAR EN LA ATRACCIÓN. Te faltan " + cm_restantes + " cm de altura.");
         }
+
+        System.out.println("Introduce tu peso (kg): ");
+        double peso = read.nextDouble();
+
+        double peso_minimo = (altura * 2.0 / 8) ;
+
+        if (peso >= peso_minimo) {
+            System.out.println("Pesas " + peso + " kg. Peso mínimo calculado: " + peso_minimo + " kg.");
+            System.out.println("¡¡SUBE A LA ATRACCIÓN!!");
+        }
+        else {
+            System.out.println("LO SIENTO. Pesas menos " + (peso_minimo - peso) + " kg menos del mínimo para esta atracción." );
+        }
+
     }
 }
