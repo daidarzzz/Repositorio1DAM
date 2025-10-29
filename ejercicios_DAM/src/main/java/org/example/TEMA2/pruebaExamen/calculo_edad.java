7c05997c0e05d3a4af34f113be5d0833f9d1a588
A701\dam1 <davidandreica127@gmail.com>
2025-10-29 20:42:10 +0100

    examen

--- ejercicios_DAM/src/main/java/org/example/TEMA2/pruebaExamen/calculo_edad.java
+++ ejercicios_DAM/src/main/java/org/example/TEMA2/pruebaExamen/calculo_edad.java
@@ -6,8 +6,13 @@ public class calculo_edad {
     static void main() {
         Scanner read = new Scanner(System.in);
         final int ANYO_ACTUAL = 2025;
+        final int ANYO_MINIMO = 1900;
         System.out.println("Permiteme preguntarte, qué año naciste");
         int anyo = read.nextInt();
+        if (anyo < ANYO_MINIMO) {
+            System.out.println("Debe ser un número del 1900 para arriba");
+            return;
+        }
         int longitud = ANYO_ACTUAL - anyo;
         System.out.println(longitud);
         for (int i = 0; i <= longitud ; i++) {
